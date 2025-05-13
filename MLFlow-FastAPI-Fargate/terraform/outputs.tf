3232a5a8283ab59070b3a8795f3fd4098e5997ed
tuangatech <anhtuantran@gmail.com>
2025-05-13 15:29:53 -0400

    Output the Role ARN

--- MLFlow-FastAPI-Fargate/terraform/outputs.tf
+++ MLFlow-FastAPI-Fargate/terraform/outputs.tf
@@ -7,3 +7,8 @@ output "ecs_task_definition" {
   description = "ECS Task Definition ARN"
   value       = aws_ecs_task_definition.fastapi_task.arn
 }
+
+# Output the Role ARN
+output "ecs_task_role_arn" {
+  value = aws_iam_role.ecs_task_role.arn
+}
